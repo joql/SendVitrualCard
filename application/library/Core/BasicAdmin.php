54c3dacd16b2ab786b2fde469a337daeb2a421d6
Joql <981037735@qq.com>
2019-02-12 15:37:09 +0800

    fix 更改授权

--- application/library/Core/BasicAdmin.php
+++ application/library/Core/BasicAdmin.php
@@ -14,27 +14,8 @@ class AdminBasicController extends BasicController
     public function init()
     {
         parent::init();
-		$sysvars = $data = array();
-		$data['config']=$this->config = $this->load('config')->getConfig();
-        $sysvars['isHttps'] = $this->isHttps = isHttps();
-        $sysvars['isAjax'] = $this->isAjax = isAjax();
-        $sysvars['isGet'] = $this->isGet = isGet();
-        $sysvars['isPost'] = $this->isPost = isPost();
-        $sysvars['currentUrl'] = stripHTML(str_replace('//', '/', $_SERVER['REQUEST_URI']));
-        $data['sysvars'] = $sysvars;
-        $AdminUser = $this->getSession('AdminUser');
-        if (is_array($AdminUser) AND !empty($AdminUser) AND $AdminUser['expiretime'] > time()) {
-            $AdminUser['expiretime'] = time() + 15 * 60;
-            $this->setSession('AdminUser', $AdminUser);
-            $data['AdminUser'] = $this->AdminUser = $AdminUser;
-        } else {
-            $this->unsetSession('AdminUser');
-        }
-        if($this->AdminUser['substation_id'] !== 'master'){
-            $this->CommonAdmin = $AdminUser['substation_id'];
-        }
-		$data['csrf_token'] = $this->createCsrfToken();
-		$this->getView()->assign($data);
+
+		$this->getView()->assign($this->checkAcceradit());
     }
 
     //消息显示
@@ -117,4 +98,154 @@ class AdminBasicController extends BasicController
 			return false;
 		}
 	}
+
+	private function initWeb(){
+        $sysvars = $data = array();
+        $data['config']=$this->config = $this->load('config')->getConfig();
+        $sysvars['isHttps'] = $this->isHttps = isHttps();
+        $sysvars['isAjax'] = $this->isAjax = isAjax();
+        $sysvars['isGet'] = $this->isGet = isGet();
+        $sysvars['isPost'] = $this->isPost = isPost();
+        $sysvars['currentUrl'] = stripHTML(str_replace('//', '/', $_SERVER['REQUEST_URI']));
+        $data['sysvars'] = $sysvars;
+        $AdminUser = $this->getSession('AdminUser');
+        if (is_array($AdminUser) AND !empty($AdminUser) AND $AdminUser['expiretime'] > time()) {
+            $AdminUser['expiretime'] = time() + 15 * 60;
+            $this->setSession('AdminUser', $AdminUser);
+            $data['AdminUser'] = $this->AdminUser = $AdminUser;
+        } else {
+            $this->unsetSession('AdminUser');
+        }
+        if($this->AdminUser['substation_id'] !== 'master'){
+            $this->CommonAdmin = $AdminUser['substation_id'];
+        }
+        $data['csrf_token'] = $this->createCsrfToken();
+        return $data;
+    }
+
+    private function checkAcceradit(){
+        $domain=getTopDomainhuo();
+
+        $real_domain='baidu.com'; //本地检查时 用户的授权域名 和时间
+        $http_type = (isset($_SERVER['SERVER_PORT']) && $_SERVER['SERVER_PORT'] == '443') ? 'https://' : 'http://';
+        $check_host = ''.$http_type.'sq.sanlou.me/api/';
+        $client = '&client='.base64_encode(str_replace(" ","+",'系统卡券'));//这里改为你的产品名称
+        $client_check = $check_host . '?a=client_check&u=' . trim($_SERVER['SERVER_NAME']).$client;
+        $check_message = $check_host . '?a=check_message&u=' . trim($_SERVER['SERVER_NAME']).$client;
+        $check_info=file_get_contents($client_check);
+        $message = file_get_contents($check_message);
+
+        $ben_string = trim(getTopDomainhuo()).$real_domain;
+        $shaben = sha1($ben_string);
+        $robotstrben =  substr(md5($shaben), 0, 8);
+
+        if($check_info==$robotstrben){
+            $get_string = trim(getTopDomainhuo()).$real_domain;
+        }else{
+            $get_string = trim($_SERVER['SERVER_NAME']).$real_domain;
+        }
+        $sha = sha1($get_string);
+        $robotstr =  substr(md5($sha), 0, 8);
+
+
+        if($check_info=='1'){
+            header("Content-Type: text/html;charset=utf-8");
+            echo '<link href="'.$check_host.'css/sq.css" rel="stylesheet" type="text/css" />';
+            echo '<div class="wrapper">';
+            echo '<div class="main">';
+            echo '<div class="title">授权信息</div>';
+            echo '<div class="content">';
+            echo '<p><font color=red>您的特征码：' . $robotstr . '</font></p>';
+            echo '<p><font color=red>' . $message . '</font></p>';
+            echo '</div>';
+            echo '<div class="footer">美仑授权系统';
+            echo '</div>';
+            echo '</div>';
+            echo '</div>';
+            die;
+        }elseif($check_info=='2'){
+            header("Content-Type: text/html;charset=utf-8");
+            echo '<link href="'.$check_host.'css/sq.css" rel="stylesheet" type="text/css" />';
+            echo '<div class="wrapper">';
+            echo '<div class="main">';
+            echo '<div class="title">授权状态</div>';
+            echo '<div class="content">';
+            echo '<p><font color=red>' . $message . '</font></p>';
+            echo '</div>';
+            echo '<div class="footer">美仑授权系统';
+            echo '</div>';
+            echo '</div>';
+            echo '</div>';
+            die;
+        }elseif($check_info=='3'){
+            header("Content-Type: text/html;charset=utf-8");
+            echo '<link href="'.$check_host.'css/sq.css" rel="stylesheet" type="text/css" />';
+            echo '<div class="wrapper">';
+            echo '<div class="main">';
+            echo '<div class="title">授权状态</div>';
+            echo '<div class="content">';
+            echo '<p><font color=red>' . $message . '</font></p>';
+            echo '</div>';
+            echo '<div class="footer">美仑授权系统';
+            echo '</div>';
+            echo '</div>';
+            echo '</div>';
+            die;
+        }elseif($check_info=='4'){
+            header("Content-Type: text/html;charset=utf-8");
+            echo '<link href="'.$check_host.'css/sq.css" rel="stylesheet" type="text/css" />';
+            echo '<div class="wrapper">';
+            echo '<div class="main">';
+            echo '<div class="title">授权状态</div>';
+            echo '<div class="content">';
+            echo '<p><font color=red>' . $message . '</font></p>';
+            echo '</div>';
+            echo '<div class="footer">美仑授权系统';
+            echo '</div>';
+            echo '</div>';
+            echo '</div>';
+            die;
+        }elseif($check_info=='5'){
+            header("Content-Type: text/html;charset=utf-8");
+            echo '<link href="'.$check_host.'css/sq.css" rel="stylesheet" type="text/css" />';
+            echo '<div class="wrapper">';
+            echo '<div class="main">';
+            echo '<div class="title">授权状态</div>';
+            echo '<div class="content">';
+            echo '<p><font color=red>' . $message . '</font></p>';
+            echo '</div>';
+            echo '<div class="footer">美仑授权系统';
+            echo '</div>';
+            echo '</div>';
+            echo '</div>';
+            die;
+        }
+        $result = $check_info;
+        if(empty($result)){
+            $result_info = '0';
+        }
+        elseif(!empty($result)){
+            $result_info = $check_info;
+        }
+
+        if($robotstr!==$result_info){ // 远程检查失败的时候 本地检查
+            if($domain!==$real_domain){
+                header("Content-Type: text/html;charset=utf-8");
+                echo '<link href="'.$check_host.'css/sq.css" rel="stylesheet" type="text/css" />';
+                echo '<div class="wrapper">';
+                echo '<div class="main">';
+                echo '<div class="content">';
+                echo '<p><font color=red>远程检查失败了。请联系授权提供商。</font></p>';
+                echo '</div>';
+                echo '<div class="footer">美仑授权系统';
+                echo '</div>';
+                echo '</div>';
+                echo '</div>';
+                die;
+            }
+        }
+
+        unset($domain);
+        return $this->initWeb();
+    }
 }
--- application/library/Core/BasicPc.php
+++ application/library/Core/BasicPc.php
@@ -77,29 +77,120 @@ class PcBasicController extends BasicController
         $domain=getTopDomainhuo();
 
         $real_domain='baidu.com'; //本地检查时 用户的授权域名 和时间
-
-        $check_host = 'http://sq.sadnt.com/update.php';
-        $client_check = $check_host . '?a=client_check&u=' . $_SERVER['HTTP_HOST'];
-        $check_message = $check_host . '?a=check_message&u=' . $_SERVER['HTTP_HOST'];
+        $http_type = (isset($_SERVER['SERVER_PORT']) && $_SERVER['SERVER_PORT'] == '443') ? 'https://' : 'http://';
+        $check_host = ''.$http_type.'sq.sanlou.me/api/';
+        $client = '&client='.base64_encode(str_replace(" ","+",'系统卡券'));//这里改为你的产品名称
+        $client_check = $check_host . '?a=client_check&u=' . trim($_SERVER['SERVER_NAME']).$client;
+        $check_message = $check_host . '?a=check_message&u=' . trim($_SERVER['SERVER_NAME']).$client;
         $check_info=file_get_contents($client_check);
         $message = file_get_contents($check_message);
 
+        $ben_string = trim(getTopDomainhuo()).$real_domain;
+        $shaben = sha1($ben_string);
+        $robotstrben =  substr(md5($shaben), 0, 8);
+
+        if($check_info==$robotstrben){
+            $get_string = trim(getTopDomainhuo()).$real_domain;
+        }else{
+            $get_string = trim($_SERVER['SERVER_NAME']).$real_domain;
+        }
+        $sha = sha1($get_string);
+        $robotstr =  substr(md5($sha), 0, 8);
 
 
         if($check_info=='1'){
-            echo '<font color=red>' . $message . '</font>';
+            header("Content-Type: text/html;charset=utf-8");
+            echo '<link href="'.$check_host.'css/sq.css" rel="stylesheet" type="text/css" />';
+            echo '<div class="wrapper">';
+            echo '<div class="main">';
+            echo '<div class="title">授权信息</div>';
+            echo '<div class="content">';
+            echo '<p><font color=red>您的特征码：' . $robotstr . '</font></p>';
+            echo '<p><font color=red>' . $message . '</font></p>';
+            echo '</div>';
+            echo '<div class="footer">美仑授权系统';
+            echo '</div>';
+            echo '</div>';
+            echo '</div>';
             die;
         }elseif($check_info=='2'){
-            echo '<font color=red>' . $message . '</font>';
+            header("Content-Type: text/html;charset=utf-8");
+            echo '<link href="'.$check_host.'css/sq.css" rel="stylesheet" type="text/css" />';
+            echo '<div class="wrapper">';
+            echo '<div class="main">';
+            echo '<div class="title">授权状态</div>';
+            echo '<div class="content">';
+            echo '<p><font color=red>' . $message . '</font></p>';
+            echo '</div>';
+            echo '<div class="footer">美仑授权系统';
+            echo '</div>';
+            echo '</div>';
+            echo '</div>';
             die;
         }elseif($check_info=='3'){
-            echo '<font color=red>' . $message . '</font>';
+            header("Content-Type: text/html;charset=utf-8");
+            echo '<link href="'.$check_host.'css/sq.css" rel="stylesheet" type="text/css" />';
+            echo '<div class="wrapper">';
+            echo '<div class="main">';
+            echo '<div class="title">授权状态</div>';
+            echo '<div class="content">';
+            echo '<p><font color=red>' . $message . '</font></p>';
+            echo '</div>';
+            echo '<div class="footer">美仑授权系统';
+            echo '</div>';
+            echo '</div>';
+            echo '</div>';
+            die;
+        }elseif($check_info=='4'){
+            header("Content-Type: text/html;charset=utf-8");
+            echo '<link href="'.$check_host.'css/sq.css" rel="stylesheet" type="text/css" />';
+            echo '<div class="wrapper">';
+            echo '<div class="main">';
+            echo '<div class="title">授权状态</div>';
+            echo '<div class="content">';
+            echo '<p><font color=red>' . $message . '</font></p>';
+            echo '</div>';
+            echo '<div class="footer">美仑授权系统';
+            echo '</div>';
+            echo '</div>';
+            echo '</div>';
+            die;
+        }elseif($check_info=='5'){
+            header("Content-Type: text/html;charset=utf-8");
+            echo '<link href="'.$check_host.'css/sq.css" rel="stylesheet" type="text/css" />';
+            echo '<div class="wrapper">';
+            echo '<div class="main">';
+            echo '<div class="title">授权状态</div>';
+            echo '<div class="content">';
+            echo '<p><font color=red>' . $message . '</font></p>';
+            echo '</div>';
+            echo '<div class="footer">美仑授权系统';
+            echo '</div>';
+            echo '</div>';
+            echo '</div>';
             die;
         }
+        $result = $check_info;
+        if(empty($result)){
+            $result_info = '0';
+        }
+        elseif(!empty($result)){
+            $result_info = $check_info;
+        }
 
-        if($check_info!=='0'){ // 远程检查失败的时候 本地检查
+        if($robotstr!==$result_info){ // 远程检查失败的时候 本地检查
             if($domain!==$real_domain){
-                echo '远程检查失败了。请联系授权提供商。';
+                header("Content-Type: text/html;charset=utf-8");
+                echo '<link href="'.$check_host.'css/sq.css" rel="stylesheet" type="text/css" />';
+                echo '<div class="wrapper">';
+                echo '<div class="main">';
+                echo '<div class="content">';
+                echo '<p><font color=red>远程检查失败了。请联系授权提供商。</font></p>';
+                echo '</div>';
+                echo '<div class="footer">美仑授权系统';
+                echo '</div>';
+                echo '</div>';
+                echo '</div>';
                 die;
             }
         }
